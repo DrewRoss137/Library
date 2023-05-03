826b49fc1cc200e80b114f620fed9973fed7f284
DrewRoss137 <DrewRoss137@Gmail.Com>
2023-05-03 20:13:17 +0100

    Implement object methods within book management functionality

--- scripts.js
+++ scripts.js
@@ -38,6 +38,90 @@ const deleteBookMenuContinueButton = document.getElementById(
 );
 const addBookButton = document.getElementById("add-books-button");
 
+const library = [
+  {
+    title: "The Pragmatic Programmer: Your Journey to Mastery",
+    author: "David Thomas, Andrew Hunt",
+    pages: 352,
+    publishDate: "2019-12-02",
+    acquisitionDate: "2023-01-23",
+    status: "Unread",
+  },
+  {
+    title: "Clean Code: A Handbook of Agile Software Craftsmanship",
+    author: "Robert C. Martin",
+    pages: 464,
+    publishDate: "2008-08-01",
+    acquisitionDate: "2023-02-01",
+    status: "Unread",
+  },
+  {
+    title: "Code Complete: A Practical Handbook of Software Construction",
+    author: "Steve McConnell",
+    pages: 960,
+    publishDate: "2004-06-09",
+    acquisitionDate: "2023-02-14",
+    status: "Unread",
+  },
+  {
+    title: "Refactoring: Improving the Design of Existing Code",
+    author: "Martin Fowler",
+    pages: 448,
+    publishDate: "2019-01-02",
+    acquisitionDate: "2023-03-19",
+    status: "Read",
+  },
+  {
+    title: "The Mythical Man-Month: Essays on Software Engineering",
+    author: "Frederick Brooks Jr.",
+    pages: 336,
+    publishDate: "1995-08-15",
+    acquisitionDate: "2023-03-23",
+    status: "Unread",
+  },
+  {
+    title: "The Clean Coder: A Code of Conduct for Professional Programmers",
+    author: "Robert C. Martin",
+    pages: 242,
+    publishDate: "2011-03-13",
+    acquisitionDate: "2023-06-21",
+    status: "Unread",
+  },
+  {
+    title: "Working Effectively with Legacy Code",
+    author: "Michael Feathers",
+    pages: 456,
+    publishDate: "2004-09-22",
+    acquisitionDate: "2023-07-27",
+    status: "Read",
+  },
+  {
+    title: "Design Patterns: Elements of Reusable Object-Oriented Software",
+    author: "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides",
+    pages: 416,
+    publishDate: "1995-03-14",
+    acquisitionDate: "2023-08-17",
+    status: "Read",
+  },
+  {
+    title:
+      "Cracking the Coding Interview: 189 Programming Questions & Solutions",
+    author: "Gayle Laakmann McDowell",
+    pages: 706,
+    publishDate: "2015-07-01",
+    acquisitionDate: "2023-12-07",
+    status: "Read",
+  },
+  {
+    title: "Soft Skills: The Software Developer's Life Manual",
+    author: "John Sonmez",
+    pages: 501,
+    publishDate: "2020-11-11",
+    acquisitionDate: "2023-12-23",
+    status: "Unread",
+  },
+];
+
 const menuItems = [
   { buttonID: "settings-button", menuID: "settings-menu" },
   { buttonID: "restore-button", menuID: "restore-menu" },
@@ -61,11 +145,24 @@ const defaultColours = {
   "tracker-colour": "#4f4557",
 };
 
+class Book {
+  constructor(title, author, pages, publishDate, acquisitionDate, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.publishDate = publishDate;
+    this.acquisitionDate = acquisitionDate;
+    this.status = status;
+  }
+}
+
 let selectedRow;
 
 menuItems.forEach(({ buttonID, menuID }) => {
-  addEventListenerToButton(buttonID, () => {
-    toggleMenu(document.getElementById(menuID));
+  const button = document.querySelector(`#${buttonID}`);
+  const menu = document.querySelector(`#${menuID}`);
+  button.addEventListener("click", () => {
+    toggleMenu(menu);
   });
 });
 
@@ -79,6 +176,22 @@ document.querySelectorAll(".close-button").forEach((closeButton) => {
 });
 
 resetButton.addEventListener("click", restoreDefaultColours);
+
+tableBody.addEventListener("click", (event) => {
+  const editButton = event.target.closest(".edit-book-button");
+  const deleteButton = event.target.closest(".delete-book-button");
+  if (editButton) {
+    selectedRow = editButton.closest("tr");
+    populateEditBookMenuInputs(selectedRow);
+    toggleMenu(editBookMenu);
+  }
+  if (deleteButton) {
+    selectedRow = deleteButton.closest("tr");
+    const rowData = getSelectedRowData(selectedRow);
+    updateDeleteBookMenuContent(rowData.bookTitleText, rowData.bookAuthorText);
+    toggleMenu(deleteBookMenu);
+  }
+});
 
 selectAllCheckbox.addEventListener("click", function () {
   for (let checkbox of selectCheckboxes) {
@@ -98,9 +211,91 @@ deleteBookMenuContinueButton.addEventListener("click", () => {
   }
 });
 
-function addEventListenerToButton(buttonID, callback) {
-  const button = document.getElementById(buttonID);
-  button.addEventListener("click", callback);
+addBookButton.addEventListener("click", function () {
+  const title = document.getElementById("add-book-title-input").value;
+  const author = document.getElementById("add-book-author-input").value;
+  const pages = document.getElementById("add-book-pages-input").value;
+  const publishDate = document.getElementById(
+    "add-book-publish-date-input"
+  ).value;
+  const acquisitionDate = document.getElementById(
+    "add-book-acquisition-date-input"
+  ).value;
+  const status = document.getElementById("add-book-status-select").value;
+  const newBook = new Book(
+    title,
+    author,
+    pages,
+    publishDate,
+    acquisitionDate,
+    status
+  );
+  createBookTableRow(newBook);
+  document.getElementById("add-book-title-input").value = "";
+  document.getElementById("add-book-author-input").value = "";
+  document.getElementById("add-book-pages-input").value = "";
+  document.getElementById("add-book-publish-date-input").value = "";
+  document.getElementById("add-book-acquisition-date-input").value = "";
+  addBookToLibrary(newBook);
+  displayBooks();
+});
+
+function addBookToLibrary(book) {
+  library.push(book);
+}
+
+function displayBooks() {
+  tableBody.innerHTML = "";
+  library.forEach((book) => {
+    createBookTableRow(book);
+  });
+}
+
+function createBookTableRow(book) {
+  const newRow = tableBody.insertRow(0);
+  newRow.classList.add("table-body-row");
+  newRow.innerHTML = `
+    ${createCell(
+      `<input class="select-checkbox" id="select-checkbox" type="checkbox" />`,
+      "book-select",
+      "book-select"
+    )}
+    ${createCell(book.title, "book-title", "book-title")}
+    ${createCell(book.author, "book-author", "book-author")}
+    ${createCell(book.pages, "book-pages", "book-pages")}
+    ${createCell(
+      formatDisplayDate(book.publishDate),
+      "book-published",
+      "book-published"
+    )}
+    ${createCell(
+      formatDisplayDate(book.acquisitionDate),
+      "book-acquired",
+      "book-acquired"
+    )}
+    ${createCell(book.status, "book-status-container", "book-status")}
+    ${createCell(
+      `
+      <a class="book-alter-button-container" id="edit-book-container" title="Edit Book">
+      <svg class="edit-book-button" id="edit-book-button" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M19.39 10.74L11 19.13V22H6C4.89 22 4 21.11 4 20V4C4 2.9 4.89 2 6 2H7V9L9.5 7.5L12 9V2H18C19.1 2 20 2.89 20 4V10.3C19.78 10.42 19.57 10.56 19.39 10.74M13 19.96V22H15.04L21.17 15.88L19.13 13.83L13 19.96M22.85 13.47L21.53 12.15C21.33 11.95 21 11.95 20.81 12.15L19.83 13.13L21.87 15.17L22.85 14.19C23.05 14 23.05 13.67 22.85 13.47Z" />
+      </svg>
+    </a>
+    <a class="book-alter-button-container" id="delete-book-container" title="Delete Book">
+      <svg class="delete-book-button" id="delete-book-button" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z"
+        />
+        </svg>
+      </a>
+        `,
+      "book-alter",
+      "book-alter"
+    )}
+    `;
+}
+
+function createCell(content, classes, id) {
+  return `<td class="${classes}" id="${id}">${content}</td>`;
 }
 
 function toggleMenu(menu) {
@@ -231,6 +426,8 @@ function updateDeleteBookMenuContent(bookTitle, bookAuthor) {
   bookAuthorText.textContent = "by " + bookAuthor.trim() + ".";
 }
 
+displayBooks();
+
 initialiseColourInput(accentColourInput, "--accent-colour");
 initialiseColourInput(backgroundColourInput, "--background-colour");
 initialiseColourInput(controlsColourInput, "--controls-colour");
@@ -240,205 +437,3 @@ initialiseColourInput(tablePrimaryColourInput, "--table-primary-colour");
 initialiseColourInput(tableSecondaryColourInput, "--table-secondary-colour");
 initialiseColourInput(textColourInput, "--text-colour");
 initialiseColourInput(trackerColourInput, "--tracker-colour");
-
-/* New Code: */
-
-function createCell(content, classes, id) {
-  return `<td class="${classes}" id="${id}">${content}</td>`;
-}
-
-tableBody.addEventListener("click", (event) => {
-  const editButton = event.target.closest(".edit-book-button");
-  const deleteButton = event.target.closest(".delete-book-button");
-  if (editButton) {
-    selectedRow = editButton.closest("tr");
-    populateEditBookMenuInputs(selectedRow);
-    toggleMenu(editBookMenu);
-  }
-  if (deleteButton) {
-    selectedRow = deleteButton.closest("tr");
-    const rowData = getSelectedRowData(selectedRow);
-    updateDeleteBookMenuContent(rowData.bookTitleText, rowData.bookAuthorText);
-    toggleMenu(deleteBookMenu);
-  }
-});
-
-addBookButton.addEventListener("click", function () {
-  const title = document.getElementById("add-book-title-input").value;
-  const author = document.getElementById("add-book-author-input").value;
-  const pages = document.getElementById("add-book-pages-input").value;
-  const publishDate = document.getElementById(
-    "add-book-publish-date-input"
-  ).value;
-  const acquisitionDate = document.getElementById(
-    "add-book-acquisition-date-input"
-  ).value;
-  const status = document.getElementById("add-book-status-select").value;
-
-  const newRow = tableBody.insertRow(0);
-  newRow.classList.add("table-body-row");
-
-  newRow.innerHTML = `
-    ${createCell(
-      `<input class="select-checkbox" id="select-checkbox" type="checkbox" />`,
-      "book-select",
-      "book-select"
-    )}
-    ${createCell(title, "book-title", "book-title")}
-    ${createCell(author, "book-author", "book-author")}
-    ${createCell(pages, "book-pages", "book-pages")}
-    ${createCell(
-      formatDisplayDate(publishDate),
-      "book-published",
-      "book-published"
-    )}
-    ${createCell(
-      formatDisplayDate(acquisitionDate),
-      "book-acquired",
-      "book-acquired"
-    )}
-    ${createCell(status, "book-status-container", "book-status")}
-    ${createCell(
-      `
-        <a class="book-alter-button-container" id="edit-book-container" title="Edit Book">
-          <svg class="edit-book-button" id="edit-book-button" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M19.39 10.74L11 19.13V22H6C4.89 22 4 21.11 4 20V4C4 2.9 4.89 2 6 2H7V9L9.5 7.5L12 9V2H18C19.1 2 20 2.89 20 4V10.3C19.78 10.42 19.57 10.56 19.39 10.74M13 19.96V22H15.04L21.17 15.88L19.13 13.83L13 19.96M22.85 13.47L21.53 12.15C21.33 11.95 21 11.95 20.81 12.15L19.83 13.13L21.87 15.17L22.85 14.19C23.05 14 23.05 13.67 22.85 13.47Z" />
-          </svg>
-        </a>
-        <a class="book-alter-button-container" id="delete-book-container" title="Delete Book">
-          <svg class="delete-book-button" id="delete-book-button" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path
-          d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z"
-          />
-          </svg>
-        </a>
-        `,
-      "book-alter",
-      "book-alter"
-    )}
-    `;
-  document.getElementById("add-book-title-input").value = "";
-  document.getElementById("add-book-author-input").value = "";
-  document.getElementById("add-book-pages-input").value = "";
-  document.getElementById("add-book-publish-date-input").value = "";
-  document.getElementById("add-book-acquisition-date-input").value = "";
-});
-
-// tableBody.addEventListener("click", (event) => {
-//   const editButton = event.target.closest(".edit-book-button");
-//   const deleteButton = event.target.closest(".delete-book-button");
-
-//   if (editButton) {
-//     selectedRow = editButton.closest("tr");
-//     populateEditBookMenuInputs(selectedRow);
-//     toggleMenu(editBookMenu);
-//   }
-
-//   if (deleteButton) {
-//     selectedRow = deleteButton.closest("tr");
-//     const rowData = getSelectedRowData(selectedRow);
-//     updateDeleteBookMenuContent(rowData.bookTitleText, rowData.bookAuthorText);
-//     toggleMenu(deleteBookMenu);
-//   }
-// });
-
-// addBookButton.addEventListener("click", function () {
-//   const title = document.getElementById("add-book-title-input").value;
-//   const author = document.getElementById("add-book-author-input").value;
-//   const pages = document.getElementById("add-book-pages-input").value;
-//   const publishDate = document.getElementById(
-//     "add-book-publish-date-input"
-//   ).value;
-//   const acquisitionDate = document.getElementById(
-//     "add-book-acquisition-date-input"
-//   ).value;
-//   const status = document.getElementById("add-book-status-select").value;
-
-//   const newRow = tableBody.insertRow(0);
-
-//   const bookSelectCell = newRow.insertCell(0);
-//   bookSelectCell.classList.add("book-select");
-//   bookSelectCell.id = "book-select";
-//   bookSelectCell.innerHTML = `
-//     <input
-//       class="select-checkbox"
-//       id="select-checkbox"
-//       type="checkbox"
-//     />
-//   `;
-
-//   const titleCell = newRow.insertCell(1);
-//   titleCell.textContent = title;
-//   titleCell.classList.add("book-title");
-//   titleCell.id = "book-title";
-
-//   const authorCell = newRow.insertCell(2);
-//   authorCell.textContent = author;
-//   authorCell.classList.add("book-author");
-//   authorCell.id = "book-author";
-
-//   const pagesCell = newRow.insertCell(3);
-//   pagesCell.textContent = pages;
-//   pagesCell.classList.add("book-pages");
-//   pagesCell.id = "book-pages";
-
-//   const publishedCell = newRow.insertCell(4);
-//   publishedCell.textContent = formatDisplayDate(publishDate);
-//   publishedCell.classList.add("book-published");
-//   publishedCell.id = "book-published";
-
-//   const acquiredCell = newRow.insertCell(5);
-//   acquiredCell.textContent = formatDisplayDate(acquisitionDate);
-//   acquiredCell.classList.add("book-acquired");
-//   acquiredCell.id = "book-acquired";
-
-//   const statusCell = newRow.insertCell(6);
-//   statusCell.textContent = status;
-//   statusCell.classList.add("book-status-container");
-//   statusCell.id = "book-status";
-
-//   const bookAlterCell = newRow.insertCell(7);
-//   bookAlterCell.classList.add("book-alter");
-//   bookAlterCell.id = "book-alter";
-//   bookAlterCell.innerHTML = `
-//   <a
-//     class="book-alter-button-container"
-//     id="edit-book-container"
-//     title="Edit Book"
-//   >
-//     <svg
-//       class="edit-book-button"
-//       id="edit-book-button"
-//       viewBox="0 0 24 24"
-//       xmlns="http://www.w3.org/2000/svg"
-//     >
-//       <path
-//         d="M19.39 10.74L11 19.13V22H6C4.89 22 4 21.11 4 20V4C4 2.9 4.89 2 6 2H7V9L9.5 7.5L12 9V2H18C19.1 2 20 2.89 20 4V10.3C19.78 10.42 19.57 10.56 19.39 10.74M13 19.96V22H15.04L21.17 15.88L19.13 13.83L13 19.96M22.85 13.47L21.53 12.15C21.33 11.95 21 11.95 20.81 12.15L19.83 13.13L21.87 15.17L22.85 14.19C23.05 14 23.05 13.67 22.85 13.47Z"
-//       />
-//     </svg>
-//   </a>
-//   <a
-//     class="book-alter-button-container"
-//     id="delete-book-container"
-//     title="Delete Book"
-//   >
-//     <svg
-//       class="delete-book-button"
-//       id="delete-book-button"
-//       viewBox="0 0 24 24"
-//       xmlns="http://www.w3.org/2000/svg"
-//     >
-//       <path
-//         d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z"
-//       />
-//     </svg>
-//   </a>
-// `;
-
-//   newRow.classList.add("table-body-row");
-
-//   document.getElementById("add-book-title-input").value = "";
-//   document.getElementById("add-book-author-input").value = "";
-//   document.getElementById("add-book-pages-input").value = "";
-//   document.getElementById("add-book-publish-date-input").value = "";
-//   document.getElementById("add-book-acquisition-date-input").value = "";
-// });
